9523f858870e55a0e5ebdfb4a279befa69608fb7
Yash Gupta <gyash24x7@gmail.com>
2020-05-01 23:20:16 +0530

    scrolling chat and submenu in channels

--- web/src/components/Chat/index.tsx
+++ web/src/components/Chat/index.tsx
@@ -1,6 +1,7 @@
-import { Card, Typography } from "antd";
-import React from "react";
+import { Card, Space, Typography } from "antd";
+import React, { useEffect, useState } from "react";
 import { DrawerContext } from "../../utils/context";
+import { stringGen } from "../../utils/lorem";
 import { PrivateLayout } from "../shared/PrivateLayout";
 import { SwitchingIcon } from "../shared/SwitchingIcon";
 import { ChannelDescription } from "./ChannelDescription";
@@ -9,31 +10,74 @@ import { MessageInput } from "./MessageInput";
 
 const { Title } = Typography;
 
+const defaultMessages = [...Array(1)].map(() => ({
+	content: stringGen.generateSentences(4),
+	by: stringGen.generateWords(2),
+	createdAt: "12:30",
+	likes: Math.round(Math.random() * 10)
+}));
+
 export const ChatScreen = () => {
+	const [messages, setMessages] = useState(defaultMessages);
+
+	const scrollToBottom = () => {
+		const container = document.querySelector(".messages-container")!;
+		const { clientHeight, scrollTop, scrollHeight } = container;
+		const newMessage = container.children[container.children.length - 1];
+		if (
+			clientHeight + scrollTop + 2 * newMessage.clientHeight >=
+			scrollHeight
+		) {
+			container.scrollTop = container.scrollHeight;
+		}
+	};
+
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
+
 	return (
 		<PrivateLayout>
 			<DrawerContext.Consumer>
 				{(props) => (
 					<Card
 						extra={
-							<span
-								onClick={() => {
-									props!.setDrawerProps!({
-										title: <Title level={4}>House Of Lords</Title>,
-										className: "channel-drawer"
-									});
-									props!.setDrawerComponent(<ChannelDescription />);
-								}}
-							>
-								<SwitchingIcon className="icon" name="info" />
-							</span>
+							<Space>
+								<span
+									onClick={() =>
+										setMessages(
+											messages.concat([
+												{
+													content: stringGen.generateSentences(4),
+													by: stringGen.generateWords(2),
+													createdAt: "12:30",
+													likes: Math.round(Math.random() * 10)
+												}
+											])
+										)
+									}
+								>
+									New Message
+								</span>
+								<span
+									onClick={() => {
+										props!.setDrawerProps!({
+											title: <Title level={4}>House Of Lords</Title>,
+											className: "channel-drawer"
+										});
+										props!.setDrawerComponent(<ChannelDescription />);
+									}}
+								>
+									<SwitchingIcon className="icon" name="info" />
+								</span>
+							</Space>
 						}
 						title={<Title level={3}>House Of Lords</Title>}
 						className="message-screen"
 					>
 						<Card.Grid className="messages-container" hoverable={false}>
-							{[...Array(20)].map((_, i) => (
-								<Message key={i} />
+							{messages.map((message) => (
+								<Message key={message.by} message={message} />
 							))}
 						</Card.Grid>
 						<Card.Grid className="message-input-container" hoverable={false}>
--- web/src/components/Navigation/ChannelsMenu.tsx
+++ web/src/components/Navigation/ChannelsMenu.tsx
@@ -2,20 +2,21 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { stringGen } from "../../utils/lorem";
 
 export const ChannelsMenu = () => {
 	const { channelId } = useParams();
 	const history = useHistory();
 
 	return (
-		<Menu selectedKeys={[channelId!]}>
+		<Menu selectedKeys={[channelId!]} mode="inline">
 			<Menu.ItemGroup title="Actions">
 				<Menu.Item>
 					<PlusCircleOutlined className="icon nav-icon" />
 					<span>New Channel</span>
 				</Menu.Item>
 			</Menu.ItemGroup>
-			<Menu.ItemGroup title="Channels">
+			<Menu.SubMenu title="Channels">
 				{channels.map((channel) => (
 					<Menu.Item
 						key={channel.id}
@@ -24,14 +25,12 @@ export const ChannelsMenu = () => {
 						<span>{channel.name}</span>
 					</Menu.Item>
 				))}
-			</Menu.ItemGroup>
+			</Menu.SubMenu>
 		</Menu>
 	);
 };
 
-const channels = [
-	{
-		id: "lnvkjfsnv",
-		name: "House of Lords"
-	}
-];
+const channels = [...Array(15)].map((_, id) => ({
+	id,
+	name: stringGen.generateWords(3)
+}));
