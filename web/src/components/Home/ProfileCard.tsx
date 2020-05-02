a69a214c28e9158cd4911d569a1c12a78781078e
Yash Gupta <gyuapstha@Yashs-MacBook-Pro.local>
2020-05-02 13:58:04 +0530

    homepage improvements

--- web/src/components/Home/ProfileCard.tsx
+++ web/src/components/Home/ProfileCard.tsx
@@ -1,29 +1,32 @@
-import { Avatar, Card } from "antd";
+import { Card, List, Typography } from "antd";
 import React from "react";
-import { Link } from "react-router-dom";
-import { SwitchingIcon } from "../shared/SwitchingIcon";
 import { UserCard } from "../shared/UserCard";
+
+const { Text, Paragraph } = Typography;
 
 export const ProfileCard = () => {
 	return (
 		<Card
 			className="profile-card"
-			cover={<img alt="" src="https://source.unsplash.com/featured/500x400" />}
-			actions={[
-				<Link to="/settings">
-					<SwitchingIcon name="settings" />
-				</Link>,
-				<SwitchingIcon name="edit" />,
-				<SwitchingIcon name="ellipsis" />
-			]}
+			cover={<img alt="" src="https://source.unsplash.com/featured/500x350" />}
 		>
-			<div className="avatar">
-				<Avatar
-					src="https://shaastra-2020.s3.ap-south-1.amazonaws.com/images/user2.svg"
-					size={100}
-				/>
-			</div>
-			<UserCard />
+			<UserCard withAvatar avatarSize={100} />
+			<br />
+			<List
+				itemLayout="horizontal"
+				dataSource={[
+					{ label: "Mobile", value: "7388378834" },
+					{ label: "Email", value: "gyash@shaastra.org" },
+					{ label: "Roll Number", value: "CH16B025" },
+					{ label: "UPI", value: "gyash24x7@okaxis" }
+				]}
+				renderItem={(item) => (
+					<List.Item>
+						<Text strong>{item.label}</Text>
+						<Paragraph>{item.value}</Paragraph>
+					</List.Item>
+				)}
+			/>
 		</Card>
 	);
 };
