7299cc9473c9f397640dff31aa9db6a114ddf504
Yash Gupta <gyash24x7@gmail.com>
2020-06-09 12:45:27 +0530

    improved mobile layout

--- web/src/components/shared/PublicLayout.tsx
+++ web/src/components/shared/PublicLayout.tsx
@@ -10,15 +10,15 @@ interface LayoutProps {
 export const PublicLayout = (props: LayoutProps) => {
 	return (
 		<Row className="wrapper" align="middle" justify="end">
-			<Col xs={24} md={15} lg={12} xl={9} xxl={8} className="public-container">
+			<Col span={9} className="public-container">
 				<div className="logo-container">
 					<img src={LightLogo} alt="Shaastra Logo" />
 				</div>
-				<div className="fixed-logo">
-					<img src={DarkLogo} alt="Shaastra Logo" />
-				</div>
 				{props.children}
 			</Col>
+			<div className="fixed-logo">
+				<img src={DarkLogo} alt="Shaastra Logo" />
+			</div>
 		</Row>
 	);
 };
